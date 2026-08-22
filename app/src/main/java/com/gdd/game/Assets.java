821a6c7553a85c739c9cb79bf6a6b8c374e87cdf
Alessandro Petrella <248337353+alpetrella@users.noreply.github.com>
2026-08-22 17:47:08 +0200

    refactor: add new assets and graphics to main menu screen

--- app/src/main/java/com/gdd/game/Assets.java
+++ app/src/main/java/com/gdd/game/Assets.java
@@ -15,9 +15,11 @@ public class Assets {
     public static final String APPNAME = "AntColony";
     private static final String ANT_BITMAP_PATH = "ant_32x32.png";
     private static final String NEST_BITMAP_PATH = "nest_128x128.png";
-    private static final String FOOD_BITMAP_PATH = "sugar_cube_32x32.png";
+    private static final String FOOD_BITMAP_PATH = "sugar_cube_64x64.png";
     private static final String WASP_BITMAP_PATH = "wasp_32x32.png";
     private static final String TERRAIN_BITMAP_PATH = "Terrain.png";
+    private static final String MAINBG_BITMAP_PATH = "menu_bg_landscape_1280x720.png";
+    private static final String MAINTITLE_BITMAP_PATH = "title_ant_colony.png";
 
 
     public static void load(AssetManager manager) {
@@ -60,18 +62,39 @@ public class Assets {
             throw new RuntimeException("Assets loading failed: ", e);
         }
 
-        if (WASP_BITMAP== null) {
+        if (WASP_BITMAP == null) {
             throw new RuntimeException("Can't load bitmap: " + WASP_BITMAP_PATH);
+        }
+
+        try (var stream = manager.open(MAINBG_BITMAP_PATH)) {
+            MAINBG_BITMAP = BitmapFactory.decodeStream(stream);
+        } catch (IOException e) {
+            throw new RuntimeException("Assets loading failed: ", e);
+        }
+
+        if (MAINBG_BITMAP == null) {
+            throw new RuntimeException("Can't load bitmap: " + MAINBG_BITMAP_PATH);
+        }
+
+        try (var stream = manager.open(MAINTITLE_BITMAP_PATH)) {
+            MAINTITLE_BITMAP = BitmapFactory.decodeStream(stream);
+        } catch (IOException e) {
+            throw new RuntimeException("Assets loading failed: ", e);
+        }
+
+        if (MAINTITLE_BITMAP == null) {
+            throw new RuntimeException("Can't load bitmap: " + MAINTITLE_BITMAP);
         }
 
         loaded = true;
     }
 
     public static Bitmap ANT_BITMAP;
-    // TODO: find some sprites for these
     public static Bitmap NEST_BITMAP;
     public static Bitmap FOOD_BITMAP;
     public static Bitmap WASP_BITMAP;
     public static Bitmap TERRAIN_BITMAP;
+    public static Bitmap MAINBG_BITMAP;
+    public static Bitmap MAINTITLE_BITMAP;
 
 }
--- app/src/main/java/com/gdd/game/screen/MainMenuScreen.java
+++ app/src/main/java/com/gdd/game/screen/MainMenuScreen.java
@@ -1,9 +1,12 @@
 package com.gdd.game.screen;
 
 import android.graphics.Canvas;
+import android.graphics.Paint;
+import android.graphics.RectF;
 
 import com.badlogic.androidgames.framework.Input;
 import com.badlogic.androidgames.framework.impl.TouchHandler;
+import com.gdd.game.Assets;
 import com.gdd.game.Game;
 import com.gdd.game.Settings;
 import com.gdd.game.ui.Panel;
@@ -17,12 +20,26 @@ public class MainMenuScreen extends Screen {
     private Canvas canvas;
     private TouchHandler touchHandler;
 
+    private final Paint paint;
+    private RectF rectBG = new RectF();
+    private RectF rectT = new RectF();
+    private float fbWidth, fbHeight;
+
     public MainMenuScreen(Game game) {
         super(game);
 
         canvas = new Canvas(game.getFramebuffer());
+        paint = new Paint(Paint.FILTER_BITMAP_FLAG);
+
+        fbWidth = game.getFramebuffer().getWidth();
+        fbHeight = game.getFramebuffer().getHeight();
+        rectBG.set(0, 0, fbWidth, fbHeight);
+        float offsetY = 50;
+        rectT.set( (fbWidth/2)-195, 0+offsetY, (fbWidth/2)+195, 78+offsetY);
+
         touchHandler = game.getTouchHandler();
         uiController = game.getUiController();
+
         initUI();
     }
 
@@ -38,6 +55,10 @@ public class MainMenuScreen extends Screen {
 
     public void render() {
         canvas.drawARGB(255, 200, 200, 200);
+
+        canvas.drawBitmap(Assets.MAINBG_BITMAP, null, rectBG, paint);
+        canvas.drawBitmap(Assets.MAINTITLE_BITMAP, null, rectT, paint);
+
         uiController.draw(canvas);
     }
 
